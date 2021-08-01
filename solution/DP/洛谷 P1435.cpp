3b7f781699e74afe2e1ce443662e5ef74749a663
1282279732 <74903660+1282279732@users.noreply.github.com>
2021-08-01 14:24:17 +0800

    [IOI2000]回文字串 / [蓝桥杯2016省]密码脱落

--- solution/DP/洛谷 P1435.cpp
+++ solution/DP/洛谷 P1435.cpp
@@ -46,3 +46,34 @@ int main()
 参考资料：
 https://blog.csdn.net/weixin_40673608/article/details/84262695?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522162779355816780269881100%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=162779355816780269881100&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~top_positive~default-1-84262695.first_rank_v2_pc_rank_v29&utm_term=%E6%9C%80%E9%95%BF%E5%85%AC%E5%85%B1%E5%AD%90%E5%BA%8F%E5%88%97&spm=1018.2226.3001.4187
 */
+
+//方法二
+//根据回文串的特点，观察首尾，判断是否插入字符
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define inf 0x3f3f3f3f
+const int maxn=1e3+5;
+
+int dp[maxn][maxn];                                     //dp[j][k]表示字符串区间j-k之间需要插入的最少字符
+char s[maxn];
+
+int main()
+{
+    scanf("%s",s+1);
+    int len=strlen(s+1);
+    for(int i=1;i<len;i++)
+    {
+        for(int j=1;j<=len-i;j++)
+        {
+            int k=i+j;
+            if(s[j]==s[k])                              //如果首尾相同取决与（j+1,k-1）区间最少插入字符数
+                dp[j][k]=dp[j+1][j-1];
+            else
+                dp[j][k]=min(dp[j+1][k],dp[j][k-1])+1;  //如果首位不同，要么头进1位或者尾退1位，插入1个字符使其首尾相同（就是+1操作），头还是尾取min即可
+        }
+    }
+    printf("%d\n",dp[1][len]);                          //最后直接输出dp【1，len】需要插入最少字符即可
+    return 0;
+}
